245ce5c9b1134e6fe7a34522b594c59ac4473c48
Isuru Fernando <isuruf@gmail.com>
2019-11-17 10:38:31 -0600

    Install irif.amdgcn.bc

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,6 +17,8 @@ cmake \
 make VERBOSE=1 -j${CPU_COUNT}
 make install
 
+cp irif/irif.amdgcn.bc $PREFIX/lib/irif.amdgcn.bc
+
 # Copy the [de]activate scripts to $PREFIX/etc/conda/[de]activate.d.
 # This will allow them to be run on environment activation.
 for CHANGE in "activate" "deactivate"
@@ -24,4 +26,3 @@ do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
     cp "${RECIPE_DIR}/activate/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/${PKG_NAME}_${CHANGE}.sh"
 done
-
